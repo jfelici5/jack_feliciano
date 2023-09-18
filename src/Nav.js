d02bb20f0acbec99a6177fe2bd0a710be623aa13
jfelici5 <jfelici5@u.rochester.edu>
2023-09-18 10:09:26 -0400

    fixed links

--- src/Nav.js
+++ src/Nav.js
@@ -29,26 +29,26 @@ function Nav() {
     <>
         <div className = "nav-left">
         <hr></hr>
-        <Link to = "/" style = {{textDecoration: 'none', color: "black"}}><div style = {{fontFamily: "Helvetica Neue", fontSize: 45 + 'px', fontStyle: "bold"}}>JACK <span style = {{fontFamily: "skinny"}}>feliciano</span></div>
+        <Link to = "/jack_feliciano/" style = {{textDecoration: 'none', color: "black"}}><div style = {{fontFamily: "Helvetica Neue", fontSize: 45 + 'px', fontStyle: "bold"}}>JACK <span style = {{fontFamily: "skinny"}}>feliciano</span></div>
         </Link>
         <div className = 'nav-links'>
             <h2 className = 'words'> 
-            <Link to = "/" style = {{textDecoration: 'none', color: "black"}}><p className = "home">Home</p></Link>
+            <Link to = "/jack_feliciano/" style = {{textDecoration: 'none', color: "black"}}><p className = "home">Home</p></Link>
             </h2> 
             <h2 className = 'words'> 
-            <Link to = "/resume" style = {{textDecoration: 'none', color: "black"}}><p className = "resume">Resume</p></Link>
-                <HashLink to = "/resume#education" style = {{textDecoration: 'none'}}><p style = {{color: "blue"}}>Education</p></HashLink>
-                <HashLink to = "/resume#skills" style = {{textDecoration: 'none'}}><p style = {{color: "blue"}}>Skills</p></HashLink>
-                <HashLink to = "/resume#experience" style = {{textDecoration: 'none'}}><p style = {{color: "blue"}}>Experience</p></HashLink>                
+            <Link to = "/jack_feliciano/resume" style = {{textDecoration: 'none', color: "black"}}><p className = "resume">Resume</p></Link>
+                <HashLink to = "/jack_feliciano/resume#education" style = {{textDecoration: 'none'}}><p style = {{color: "blue"}}>Education</p></HashLink>
+                <HashLink to = "/jack_feliciano/resume#skills" style = {{textDecoration: 'none'}}><p style = {{color: "blue"}}>Skills</p></HashLink>
+                <HashLink to = "/jack_feliciano/resume#experience" style = {{textDecoration: 'none'}}><p style = {{color: "blue"}}>Experience</p></HashLink>                
             </h2>
             {/* <h2 className = 'words'>  */}
             {/* <Link to = "/projects" style = {{textDecoration: 'none', color: "black"}}><p className = "projects">Projects</p></Link>            <p style = {{color: "blue"}}>2048</p>
             <p style = {{color: "blue"}}>urafterhours</p>
             </h2> */}
             <h2 className = 'words'> 
-            <Link to = "/photos" style = {{textDecoration: 'none', color: "black"}}><p className = "gallery">Blogs</p></Link>            
-            <Link to = "/photos/incline" style = {{textDecoration: 'none'}}><p  className = "city"style = {{color: "blue"}}>Incline Village</p></Link>
-            <Link to = "/photos/usopen" style = {{textDecoration: 'none'}}><p  className = "city"style = {{color: "blue"}}>US Open</p></Link>
+            <Link to = "/jack_feliciano/photos" style = {{textDecoration: 'none', color: "black"}}><p className = "gallery">Blogs</p></Link>            
+            <Link to = "/jack_feliciano/photos/incline" style = {{textDecoration: 'none'}}><p  className = "city"style = {{color: "blue"}}>Incline Village</p></Link>
+            <Link to = "/jack_feliciano/photos/usopen" style = {{textDecoration: 'none'}}><p  className = "city"style = {{color: "blue"}}>US Open</p></Link>
 
             </h2>
             {/* <h2 className = 'words'> 
